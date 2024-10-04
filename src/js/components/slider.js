395f17e5708515cd89701ffa19549df141e23ae0
Artyushenko_Daniil <foxones80@gmail.com>
2024-10-04 23:35:04 +0600

    first commit

---
+++ src/js/components/slider.js
@@ -0,0 +1,17 @@
+// import { Swiper } from "swiper";
+// import { Autoplay } from "swiper/modules";
+
+// Swiper.use([Autoplay]);
+// window.addEventListener("DOMContentLoaded", (e) => {
+//   new Swiper(".gallery__slider", {
+//     slidesPerView: "auto",
+//     loop: true,
+//     speed: 5000,
+//     autoplay: {
+//       delay: 0,
+//     },
+//     easing: "linear",
+//     centeredSlides: true,
+//     spaceBetween: 10,
+//   });
+// });
