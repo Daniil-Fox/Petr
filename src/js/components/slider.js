1d8a08b51199fb946dc9582060d0b1a4cdff9779
Artyushenko_Daniil <foxones80@gmail.com>
2024-10-05 13:17:26 +0600

    fixed some

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,17 +1,42 @@
-// import { Swiper } from "swiper";
-// import { Autoplay } from "swiper/modules";
+import { Swiper } from "swiper";
+import { FreeMode, Thumbs } from "swiper/modules";
 
-// Swiper.use([Autoplay]);
-// window.addEventListener("DOMContentLoaded", (e) => {
-//   new Swiper(".gallery__slider", {
-//     slidesPerView: "auto",
-//     loop: true,
-//     speed: 5000,
-//     autoplay: {
-//       delay: 0,
-//     },
-//     easing: "linear",
-//     centeredSlides: true,
-//     spaceBetween: 10,
-//   });
-// });
+Swiper.use([Thumbs, FreeMode]);
+window.addEventListener("DOMContentLoaded", (e) => {
+  // new Swiper(".gallery__slider", {
+  //   slidesPerView: "auto",
+  //   loop: true,
+  //   speed: 5000,
+  //   autoplay: {
+  //     delay: 0,
+  //   },
+  //   easing: "linear",
+  //   centeredSlides: true,
+  //   spaceBetween: 10,
+  // });
+  const thumbsSlider = new Swiper(".interview__thumbs", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    breakpoints: {
+      320: {
+        slidesPerView: "auto",
+        spaceBetween: 8,
+        freeMode: {
+          enabled: true,
+        },
+      },
+      769: {
+        freeMode: false,
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+    },
+  });
+  const interviewSlider = new Swiper(".interview__slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    thumbs: {
+      swiper: thumbsSlider,
+    },
+  });
+});
--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,29 @@ import "./_components.js";
 
 const menu = document.querySelector(".menu");
 const menuBtn = document.querySelector(".header__burger");
-
+const menuBody = menu?.querySelector(".menu__body");
 menuBtn?.addEventListener("click", (e) => {
-  menu.classList.toggle("active");
+  let isActive = menu.classList.toggle("active");
   menuBtn.classList.toggle("active");
+
+  if (isActive) {
+    menu.style.display = "flex";
+    setTimeout(() => {
+      menu.style.opacity = 1;
+      menu.style.zIndex = 100;
+    }, 10);
+
+    setTimeout(() => {
+      menuBody.style.transform = "translateX(0)";
+    }, 310);
+  } else {
+    menuBody.style.transform = null;
+    setTimeout(() => {
+      menu.style.opacity = null;
+      menu.style.zIndex = null;
+    }, 310);
+    setTimeout(() => {
+      menu.style.display = null;
+    }, 320);
+  }
 });
